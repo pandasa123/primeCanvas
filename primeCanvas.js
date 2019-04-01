dd4f718d385686fdce2f57e1f980345b7a08627f
Sanket Panda <pandasa@umich.edu>
2019-04-01 12:27:50 -0400

    Added step functionality

--- primeCanvas.js
+++ primeCanvas.js
@@ -1,17 +1,23 @@
-function setup () {
-  const amount = 144
-  const diameter = 5
-  const space = 0.5
-  const margin = 50
-  let number = 1
+var base = 1
+var amount = 1
+const diameter = 5
+const space = 0.5
+const margin = 50
 
+function setup () {
   createCanvas(
-    amount * (diameter + space) - space + 2 * margin,
-    amount * (diameter + space) - space + 2 * margin
+    windowWidth,
+    windowHeight
+    // amount * (diameter + space) - space + 2 * margin,
+    // amount * (diameter + space) - space + 2 * margin
   )
+}
+
+function draw () {
   background(255)
   fill(0, 0, 0)
   stroke(255, 255, 255)
+  let number = 1
   for (let y = 0; y < amount; y++) {
     for (let x = 0; x < amount; x++) {
       if (isPrime(number)) {
@@ -21,6 +27,18 @@ function setup () {
       }
       number++
     }
+  }
+}
+
+function keyPressed () {
+  // if (amount < 144) {
+    // setInterval((amount *= amount), 1000)
+  // }
+  if (base < 12) {
+    base += 1
+    amount = base*base
+    console.log(amount)
+    redraw()
   }
 }
 
